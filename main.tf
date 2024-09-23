ff6af188e3f3841c0dc479f42ebd8aa7c9657ad0
der <mr73367@gmail.com>
2024-09-23 19:25:44 +0700

    vpc setup

--- main.tf
+++ main.tf
@@ -18,22 +18,22 @@ module "network" {
 }
 
 //calling the ec2 module
-# module "ec2" {
-#   source     = "./_modules/ec2"
-#   depends_on = [module.network]
-#   for_each = var.bastion_definition
-#   bastion_instance_class = each.value.bastion_instance_class
-#   bastion_name           = each.value.bastion_name
-#   bastion_public_key     = each.value.bastion_public_key
-#   trusted_ips            = toset(each.value.trusted_ips)
-#   default_tags           = merge(var.tags, each.value.ext-tags)
-#   user_data_base64       = each.value.user_data_base64
-#   bastion_ami            = each.value.bastion_ami
-#   associate_public_ip_address = each.value.associate_public_ip_address
-#   public_subnet_id       = module.network.public_subnet_id[0]
-#   bastion_monitoring     = each.value.bastion_monitoring
-#   vpc_id                 = module.network.vpc_id
-# } 
+module "ec2" {
+  source     = "./_modules/ec2"
+  depends_on = [module.network]
+  for_each = var.bastion_definition
+  bastion_instance_class = each.value.bastion_instance_class
+  bastion_name           = each.value.bastion_name
+  bastion_public_key     = each.value.bastion_public_key
+  trusted_ips            = toset(each.value.trusted_ips)
+  default_tags           = merge(var.tags, each.value.ext-tags)
+  user_data_base64       = each.value.user_data_base64
+  bastion_ami            = each.value.bastion_ami
+  associate_public_ip_address = each.value.associate_public_ip_address
+  public_subnet_id       = module.network.public_subnet_id[0]
+  bastion_monitoring     = each.value.bastion_monitoring
+  vpc_id                 = module.network.vpc_id
+} 
 
 # module "s3" {
 #   count = var.create_s3_bucket ? 1 : 0
